32e5e7c49e45738c36ec4c4e8ba0383ac9947793
MaciejKut <maciej.kutzmann@gmail.com>
2019-03-23 11:13:26 +0100

    change into APP get Gif

--- components/App.js
+++ components/App.js
@@ -8,31 +8,53 @@ App = React.createClass({
             gif: {}
         };
     },
-    getGif: function (searchingText, callback) {  // 1.
-        var url = GIPHY_API_URL + '/v1/stickers/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText + '&rating=G';  // 2.
-        var xhr = new XMLHttpRequest();  // 3.
-        xhr.open('GET', url);
-        xhr.onload = function () {
-            if (xhr.status === 200) {
-                var data = JSON.parse(xhr.responseText).data; // 4.
-                var gif = {  // 5.
-                    url: data.fixed_width_downsampled_url,
-                    sourceUrl: data.url
-                };
-                callback(gif);  // 6.
-            }
-        };
-        xhr.send();
+    getGif: function (searchingText, callback) { // 1.
+        var url = GIPHY_API_URL + '/v1/stickers/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText + '&rating=G'; // 2.
+        return new Promise(function (resolve, reject) {
+            const request = new XMLHttpRequest();
+            request.onload = function () {
+                if (this.status === 200) {
+
+                    var data = JSON.parse(request.responseText).data; // 4.
+                    var gif = { // 5.
+                        url: data.fixed_width_downsampled_url,
+                        sourceUrl: data.url
+                    };
+                    callback(gif); // 6.
+
+                } else {
+                    recect(new Error(this.statusText));
+                }
+            };
+            request.onerror = function () {
+                reject(new Error(`XMLHttpRequest Error: ${this.statusText}`));
+            };
+            request.open('GET', url);
+            request.send();
+        })
+        // var xhr = new XMLHttpRequest();  // 3.
+        // xhr.open('GET', url);
+        // xhr.onload = function () {
+        //     if (xhr.status === 200) {
+        //         var data = JSON.parse(xhr.responseText).data; // 4.
+        //         var gif = {  // 5.
+        //             url: data.fixed_width_downsampled_url,
+        //             sourceUrl: data.url
+        //         };
+        //         callback(gif);  // 6.
+        //     }
+        // };
+        // xhr.send();
     },
-    handleSearch: function (searchingText) {  // 1.
+    handleSearch: function (searchingText) { // 1.
         this.setState({
-            loading: true  // 2.
+            loading: true // 2.
         });
-        this.getGif(searchingText, function (gif) {  // 3.
-            this.setState({  // 4
-                loading: false,  // a
-                gif: gif,  // b
-                searchingText: searchingText  // c
+        this.getGif(searchingText, function (gif) { // 3.
+            this.setState({ // 4
+                loading: false, // a
+                gif: gif, // b
+                searchingText: searchingText // c
             });
         }.bind(this));
     },
@@ -44,21 +66,29 @@ App = React.createClass({
             width: '90%'
         };
 
-        return (
-            <div style={styles}>
-                <h1>Wyszukiwarka GIFow!</h1>
-                <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
-                <Search
-                    onSearch={
-                        this.handleSearch
-                    }
-                />
-                <Gif
-                    loading={this.state.loading}
-                    url={this.state.gif.url}
-                    sourceUrl={this.state.gif.sourceUrl}
-                />
-            </div>
+        return ( <
+            div style = {
+                styles
+            } >
+            <
+            h1 > Wyszukiwarka GIFow! < /h1> <
+            p > Znajdź gifa na < a href = 'http://giphy.com' > giphy < /a>. Naciskaj enter, aby pobrać kolejne gify.</p >
+            <
+            Search onSearch = {
+                this.handleSearch
+            }
+            /> <
+            Gif loading = {
+                this.state.loading
+            }
+            url = {
+                this.state.gif.url
+            }
+            sourceUrl = {
+                this.state.gif.sourceUrl
+            }
+            /> < /
+            div >
         );
     }
 });
